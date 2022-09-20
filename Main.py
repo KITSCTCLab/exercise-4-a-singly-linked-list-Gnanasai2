46b70a213685495c62689e75a1ef056b53e95763
Gnanasai <110010422+Gnanasai2@users.noreply.github.com>
2022-09-20 12:09:44 +0530

    Update Main.py

--- Main.py
+++ Main.py
@@ -3,30 +3,14 @@ from typing import Optional
 
 class Node:
     """
-    Provide necessary documentation
-    """
-    def _init_(self, data=None, next=None):
-        """
-        Provide necessary documentation
-        """
-        self.data = data
-        self.next = next
-
-
-from typing import Optional
-
-
-class Node:
-    """
-    This class describes Node objects to act as elements of the LinkedList
-    Attributes:
-        -> data - stored associated data
-        -> next - link to next node
+    Provide necessary documentaion
+        data=store associated data
+        next=link to next node
     """
 
     def __init__(self, data=None, next=None):
         """
-        Initialises the Node with given attributes
+        Initializes the Node with the given attributes
         """
         self.data = data
         self.next = next
@@ -36,10 +20,10 @@ class LinkedList:
     """
     This class implements LinkedList using Node objects
     Methods:
-        -> insert_at_end - inserts node with data at the end of the list
-        -> status - displays all elements of the lisT
-    Attributes
-        -> self.head - contains first node of LinkedList, None if list empty
+        insert_at_end-inserts node with data at the end of the list
+        status-displays all elements of the list
+    Attributes:
+        self.head-contains first node of LinkedList and None if list empty
     """
 
     def __init__(self):
@@ -53,7 +37,7 @@ class LinkedList:
         Insert node at end of the list
         :param data: integer data that will be used to create a node
         """
-        new = Node(data, None)
+        new = Node(data)
         current = self.head
         if current is None:
             self.head = new
@@ -76,7 +60,8 @@ class LinkedList:
 
 class Solution:
     """
-    Class implementing functions to add numbers in a LinkedListT
+    Provide necessary documentation
+    Class implementing the functions to add numbers in a LinkedList
     
     """
 
@@ -125,3 +110,7 @@ for data in data_for_second_list:
     second_list.insert_at_end(data)
 # Create an instance for Solution
 solution = Solution()
+# Pass first_list and second_list to addTwoNumbers, which returns a new linked list
+new_list = solution.addTwoNumbers(first_list, second_list)
+# Display the status of new_list
+new_list.status()
